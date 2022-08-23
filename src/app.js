aafac3a700c1c9e69ec1c3c2a53349974dbe5ff3
Isaac Pontes <isaaclfp@outlook.com>
2022-08-23 09:25:36 -0300

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+const cors = require("cors")
 const express = require("express")
 const router = require("./router")
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(router)
 
