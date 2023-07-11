5c6b3fa798fa908cfd1fd0e02d89f04df644a8fb
Isaac Pontes <isaaclfp@outlook.com>
2023-07-11 12:23:23 -0300

    chore: update dependencies and remove nodemon

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const app = require("./app")
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, function () {
-  console.log(`Server started at port ${PORT}.`)
+  console.log(`Server started successfully! Running on http://localhost:${PORT}/`)
 })
